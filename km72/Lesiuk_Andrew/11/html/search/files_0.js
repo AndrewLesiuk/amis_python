238d0b8d3f9b856867cb4910468fed268e781efc
Lesiuk <32434083+AndrewLesiuk@users.noreply.github.com>
2017-11-26 19:36:22 +0200

    Homework
    11. Рекурсія

---
+++ km72/Lesiuk_Andrew/11/html/search/files_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['task1_2e1_2epy',['task1.1.py',['../task1_81_8py.html',1,'']]],
+  ['task1_2e2_2epy',['task1.2.py',['../task1_82_8py.html',1,'']]],
+  ['task2_2epy',['task2.py',['../task2_8py.html',1,'']]],
+  ['task3_2epy',['task3.py',['../task3_8py.html',1,'']]]
+];
